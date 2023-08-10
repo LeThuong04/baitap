05455f420fa735b444f6719ccbe9d998e44e53ba
LeThuong04 <thuongtui02335@gmail.com>
2023-08-10 10:45:54 +0700

    complete

--- index.js
+++ index.js
@@ -1,3 +1,52 @@
-alert("check");
-var name = "Anh thuong ne";
-console.log(typeof name);
+// bt1
+/*var length = prompt("Nhap chieu dai");
+ var weight = prompt("Nhap chieu rong");
+ var rectangle = length * weight;
+ alert("Dien tich hinh chu nhat la " + rectangle);*/
+// bt2
+/*var n = prompt("nhap so can xet");
+ if (n % 2 == 0) {
+   alert("so chan");
+ } else {
+   alert("so le");
+ }*/
+//bt3
+/*for (var i = 1; i <= 10; i++) {
+   alert("" + i);
+ }*/
+//bt4
+/*while
+ var i = 10; 
+ while (i > 0) {
+   alert("" + i);
+   i--;
+ }
+ do while
+ var i = 10;
+ do {
+   alert("" + i);
+   i--;
+ } while (i >= 1);*/
+//bt5
+/* var n = prompt("Nhap so  n");
+ for (var i = 0; i <= n; i++) {
+   alert("" + i);
+ }
+ var i = 0;
+ while (i <= n) {
+   alert("" + i);
+   i++;
+ }
+ var i = 0;
+ do {
+   alert("" + i);
+   i++;
+ } while (i <= n);*/
+//bt 6
+/*var n = prompt("Nhap so n: ");
+var sum = 0;
+for (var i = 0; i <= n; i++) {
+  sum += i;
+}
+alert("" + sum);*/
+// COMPLETED!
